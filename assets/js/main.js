801ff896d35eaaa11f333904591ed15cfeffd118
yasmin <yasmintolba227@gmail.com>
2024-06-02 15:58:27 +0300

    top nav

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+// toggle active class on header
+let menuBtn = document.querySelectorAll('.list-links .nav-item')
+menuBtn.forEach((li) => {
+  li.addEventListener("click", (e) => {
+    menuBtn.forEach((el) => el.classList.remove("active"));
+    li.classList.add("active");
+  });
+});
+
+
+// 
+window.onscroll = function () {
+  var top = window.pageXOffset ? window.pageXOffset : document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop;
+  var header = document.getElementById("main-header");
+  if (top > 50) {
+    header.style.position = "fixed";
+    // header.style.height = "50px";
+  } else {
+    header.style.position = "relative";
+    // header.style.height = "100px";
+  }
+}
