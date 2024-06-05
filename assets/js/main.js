d33ebfe49e7142dfc7c8fd35563b35189c52a138
yasmin <yasmintolba227@gmail.com>
2024-06-06 00:39:54 +0300

    auth pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,6 +25,64 @@ $(document).ready(function () {
   $('select').niceSelect();
 });
 
+// Toggle hide and show password
+$(document).ready(function () {
+  $(".show-hidePassword").click(function () {
+    console.log('test');
+    var passwordInput = $($(this).siblings(".password-input"));
+    if (passwordInput.attr("type") == "password") {
+      passwordInput.attr("type", "text");
+      $(this).find(".fa-eye-slash").removeClass("fa-eye-slash").addClass("fa-eye");
+    } else {
+      passwordInput.attr("type", "password");
+      $(this).find(".fa-eye").removeClass("fa-eye").addClass("fa-eye-slash");
+    }
+  });
+})
+
+function auto_tab_input() {
+  $(".code-inputs .form-control").keyup(function () {
+    if (this.value.length == this.maxLength) {
+      $(this).nextAll(".code-inputs .form-control:enabled:first").focus();
+    }
+  });
+}
+function auto_backspace() {
+  $(".code-inputs .form-control").keyup(function (e) {
+    if (e.keyCode == 8) {
+      if ($(this).prev().length > 0) {
+        $(this).prev("input").focus();
+      }
+    }
+  });
+}
+function on_Input_Func(input){
+  // console.log('done');
+  if (input.value.length > input.maxLength) input.value = input.value.slice(0, input.maxLength);
+}
+$(document).ready(function () {
+  auto_tab_input();
+  auto_backspace();
+});
+
+
+// owl carousel
+// $('.owl-carousel').owlCarousel({
+//   rtl: true,
+//   loop: true,
+//   margin: 0,
+//   dots: true,
+//   arrow: true,
+//   nav: true,
+//   items: 1,
+//   lazyLoad: true,
+//   autoplay: true,
+//   autoplaySpeed: 2000,
+//   autoplayTimeout: 5000,
+//   autoplayHoverPause: true,
+//   center: true,
+// })
+
 // owl carousel
 $('.owl-carousel').owlCarousel({
   rtl: true,
@@ -34,11 +92,7 @@ $('.owl-carousel').owlCarousel({
   arrow: true,
   nav: true,
   items: 1,
-  lazyLoad: true,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  autoplayTimeout: 5000,
-  autoplayHoverPause: true,
   center: true,
 })
+
 
